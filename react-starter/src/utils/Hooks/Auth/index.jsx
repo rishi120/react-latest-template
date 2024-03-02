ef79ef3c3e326abc2396a4a6ec70614fabcb7ab4
rishi120 <r.baruah42@gmail.com>
2024-03-02 22:32:48 +0530

    Custom hooks changes.

--- react-starter/src/utils/Hooks/Auth/index.jsx
+++ react-starter/src/utils/Hooks/Auth/index.jsx
@@ -4,12 +4,29 @@ export const useProviderAuth = createContext();
 
 export const UseLoginProvider = () => useContext(useProviderAuth);
 
+/**
+ * defined a custom hook that returns a string and function with employee details.
+ * @returns None
+ */
+
 function Uselogindata() {
     const [storePassword, setStorePassword] = useState("React Starter Template");
+    const name = ("(React Context Approach)");
+
+    const headingStyle = {
+        fontSize: "30px"
+    }
+    const spanElementStyles = {
+        fontSize: "20px",
+        paddingLeft: "5px"
+    }
 
     return {
         storePassword,
-        setStorePassword
+        setStorePassword,
+        name,
+        headingStyle,
+        spanElementStyles
     }
 }
 
